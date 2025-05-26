1945d3d7241227e97afc954be173506043b3cb3a
Gabriel Souza <Gabriel@DSV-GABRIEL>
2025-05-26 16:10:00 -0300

    melhoria nos models

---
+++ A3.Gestao.Servidor/Models/Enum/Delimitadores.cs
@@ -0,0 +1,13 @@
+﻿namespace A3.Gestao.Servidor.Models.Enum
+{
+    public enum Delimitadores
+    {
+        Nome = '$',
+        IdProduto = '$',
+        EmailVendedor = '#',
+        ValorLance = '&',
+        EmailComprador = '%',
+        Decimal = '%',
+        Texto = '&',
+    }
+}
--- A3.Gestao.Servidor/Models/Produto.cs
+++ A3.Gestao.Servidor/Models/Produto.cs
@@ -4,19 +4,35 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; } = string.Empty;
-        public string EmailVendedor { get; set; } = string.Empty;
-        public decimal MelhorLance { get; set; }
-        public string EmailClienteMelhorLance { get; set; } = string.Empty;
-        public bool Finalizado { get; set; } = false;
-        public bool TeveLances { get; set; } = false;
+        public string EmailVendedor { get; private set; } = string.Empty;
+        public decimal MelhorLance { get; private set; }
+        public string EmailClienteMelhorLance { get; private set; } = string.Empty;
+        public bool Finalizado { get; private set; } = false;
+        public bool TeveLances { get; private set; } = false;
 
+        public Produto(string nome, string emailVendedor, decimal melhorLance)
+        {
+            Nome = nome;
+            EmailVendedor = emailVendedor;
+            MelhorLance = melhorLance;
+            Validar();
+        }
+
+        private void Validar()
+        {
+            if (string.IsNullOrEmpty(Nome))
+                throw new Exception("O nome do produto deve ser informado");
+            if (string.IsNullOrEmpty(EmailVendedor))
+                throw new Exception("O nome do vendedor deve ser informado");
+            if (MelhorLance <= 0)
+                throw new Exception("O valor do produto deve ser maior que zero");
+        }
         public string FinalizarLeilao()
         {
             Finalizado = true;
             if (!TeveLances) return @$"LEILÃO FINALIZADO, O produto de ID: {Id}, Nome :{Nome}, NÃO TEVE NENHUM LANCE REGISTRADO.";
 
             return @$"LEILÃO FINALIZADO, DADOS: Id: {Id}, Nome do produto: {Nome}, Email do Comprador: {EmailClienteMelhorLance} Melhor lance: {MelhorLance}";
-
         }
 
         public void AtualizarLance(decimal valor, string emailComprador)
