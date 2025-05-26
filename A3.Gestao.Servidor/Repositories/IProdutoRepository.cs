2ff245ec271aea474187fb49726b3e17fe6b1f7e
Gabriel Souza <Gabriel@DSV-GABRIEL>
2025-05-26 15:17:23 -0300

    adicao do repositorio

---
+++ A3.Gestao.Servidor/Repositories/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using A3.Gestao.Servidor.Models;
+
+namespace A3.Gestao.Servidor.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Produto? Buscar(int id);
+        void Remover(int id);
+        void Adicionar(Produto produto);
+        IList<Produto> Listar();
+        IList<Produto> ListarFinalizados();
+
+
+    }
+}
---
+++ A3.Gestao.Servidor/Repositories/ProdutoRepository.cs
@@ -0,0 +1,33 @@
+﻿using A3.Gestao.Servidor.Models;
+
+namespace A3.Gestao.Servidor.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        public List<Produto> produtos;
+        public ProdutoRepository()
+        {
+            produtos = [];
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            var ultimo = produtos.LastOrDefault();
+            produto.Id = ultimo is null ? 0 : ultimo.Id + 1;
+            produtos.Add(produto);
+        }
+
+        public Produto? Buscar(int id) => produtos.FirstOrDefault(p => p.Id == id);
+
+        public IList<Produto> Listar() => [.. produtos];
+
+        public IList<Produto> ListarFinalizados() => produtos.Where(x => x.Finalizado).ToList();
+
+        public void Remover(int id)
+        {
+            var produto = Buscar(id);
+            if(produto is not null)
+                produtos.Remove(produto);
+        }
+    }
+}
