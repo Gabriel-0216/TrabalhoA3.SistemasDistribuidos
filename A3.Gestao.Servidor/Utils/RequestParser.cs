55d82fa4c95238fa2ab14d4af0891996662a0ace
Gabriel Souza <Gabriel@DSV-GABRIEL>
2025-05-26 15:17:15 -0300

    adicionando parser

---
+++ A3.Gestao.Servidor/Utils/RequestParser.cs
@@ -0,0 +1,14 @@
+﻿using A3.Gestao.Servidor.Models.Constante;
+
+namespace A3.Gestao.Servidor.Utils
+{
+    public static class RequestParser
+    {
+        public static string Extrair(string request, Delimitadores delimitador)
+        {
+            var inicioIndex = request.IndexOf(delimitador.Delimitador);
+            var ultimoIndex = request.LastIndexOf(delimitador.Delimitador);
+            return request.Substring(inicioIndex + 1, ultimoIndex - inicioIndex - 1);
+        }
+    }
+}
